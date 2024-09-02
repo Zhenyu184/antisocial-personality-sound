ddddd1a8e9743d1a7f9e46ba27a879e81079eef1
Zhenyu184 <zyy1120126@gmail.com>
2024-09-02 23:18:21 +0800

    feat(ui.py): add first UI code

---
+++ src/ui.py
@@ -0,0 +1,20 @@
+from PyQt6 import QtWidgets
+import sys
+
+class MyWidget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('chris.studio')
+        self.resize(300, 200)
+        self.setUpdatesEnabled(True)
+        self.ui()
+
+    def ui(self):
+        self.label = QtWidgets.QLabel(self)
+        self.label.setText('hello world')
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    Form = MyWidget()
+    Form.show()
+    sys.exit(app.exec())
